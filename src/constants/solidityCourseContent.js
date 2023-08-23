b5657d5bd780b5769637898e91179a2db2a36ae1
Amruth Kiran <amruthkiran47@gmail.com>
2023-08-23 20:55:52 +0530

    solidity course

--- src/constants/solidityCourseContent.js
+++ src/constants/solidityCourseContent.js
@@ -64,5 +64,298 @@ export const solidityCourseContent = {
         },
       ],
     },
+
+    //solidity in depth
+    {
+      title: "Solidity in Depth",
+      lecturesCount: "22",
+      totalDuration: "2 hrs",
+      lecture: [
+        {
+          title: "Solidity file layout",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/Qmd6UUhmVrSXCYoQimHfVSEQfAibRpfqPtqVZ1F57Gao9w",
+          usersCompleted: [],
+        },
+        {
+          title: "Value types in Solidity",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmUUunfgdNSZdT5oDjtAu4fmXAuAWgmAGFzNbhcfxyRu2D",
+          usersCompleted: [],
+        },
+        {
+          title: "Function Types",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/Qmbu5QKRq4VDBvHLvoEXTvMAMtSRpvPuYT4FXbXYKVXNt7",
+          usersCompleted: [],
+        },
+        {
+          title: "Reference Types",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmXUKRJL1L5phPbCGehjUTyfJg9DRzJRAt4aM19g8fxqMy",
+          usersCompleted: [],
+        },
+        {
+          title: "Mapping Types",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/Qmc2S1TmdXKVn2b1XTuweXeepYE6u7NowaeeWkKZvXri5U",
+          usersCompleted: [],
+        },
+        {
+          title: "Implicit & Explicit Conversion",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmXK9ia8jKKJS9fX74QxHYM7uB57iufh2sRHvtDQ8hBKm5",
+          usersCompleted: [],
+        },
+        {
+          title: "Variable (Variables, Memory Allocation and Encapsulation)",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmT2uDsRh82inqCymUtRqrMzD5qGxijpjq6yE9NZLviPnX",
+          usersCompleted: [],
+        },
+        {
+          title: "Modifiers",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmSCV5NJZSwN45SAA2vKFdiH8uAvenUVYNYAeSe4bqmjA2",
+          usersCompleted: [],
+        },
+        {
+          title: "Callback Functions",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmY6nLQznZtkeSqqNob8FCYZXF9qCH2uGDbq4W4M4S93mq",
+          usersCompleted: [],
+        },
+        {
+          title: "Function Overloading",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmaWr1PbcDMRsju1j2XBLNCQTQHwHEtPSAVKVqsDMFQrVG",
+          usersCompleted: [],
+        },
+        {
+          title: "Inheritance",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/Qmar6E5sRoMYXadVpwe55SP6aUzAFyunhbCBcDfxertjBw",
+          usersCompleted: [],
+        },
+        {
+          title: "Events",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmVCHA9zEXL1BZfaZcfemm5NXSinnEk2rv34GGwuHbXotN",
+          usersCompleted: [],
+        },
+        {
+          title: "Constructor",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmQbNZtbmDySazX28wyZHcftZ5m8JdrjMRgyMprSiadhAU",
+          usersCompleted: [],
+        },
+        {
+          title: "Interface & Abstract Contract",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmdK1w5cTcSfwAfSkeiWPbrb71YSHLAfgbbsWaPNPCckys",
+          usersCompleted: [],
+        },
+        {
+          title: "Error Handling: Using Require, Revert, Assert & Try/Catch",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmcwAWaPkznUNwTUxwMzs78ufmMQCL4awkJK2raVUc7o4P",
+          usersCompleted: [],
+        },
+        {
+          title: "Handling Error From External Calls Using Try & catch",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmNgpb1jgabC6dkFBKcpJyHWfXLXE9Chu7MYRu6H3GWZNA",
+          usersCompleted: [],
+        },
+        {
+          title: "Sending Ether (When to use transfer, send & call.value)",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/Qma5QTr97DaBUtBA8Asc8cYnLDPvUG8V7THjSVPbQq58Y2",
+          usersCompleted: [],
+        },
+        {
+          title: "Calling other contracts (Task Delegate Call)",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmXvLD9cbFWn7FS8te2Wu2KzT8BjmP3dJgAsBMb52Sj6oR",
+          usersCompleted: [],
+        },
+        {
+          title: "Solidity Libraries (Embedded)",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmXvLD9cbFWn7FS8te2Wu2KzT8BjmP3dJgAsBMb52Sj6oR",
+          usersCompleted: [],
+        },
+        {
+          //TODO video not available
+          title: "Linking Deployed Library in Solidity",
+          desc: "",
+          videoUrl: ".",
+          usersCompleted: [],
+        },
+        {
+          title: "Hashing",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmUmXohMZ3zJHY8m5PCfuku45p92YguygvaRjFsu6xis2X",
+          usersCompleted: [],
+        },
+        {
+          title: "Wei & Ether",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmS8nS1QugDfdEmoXkTw3Hcfsq28KrV9VyQEseDSatLT9U",
+          usersCompleted: [],
+        },
+      ],
+    },
+
+    //Build a Smart Contract to Accept Payment and Withdraw it using Truffle
+    {
+      title: "Build a Smart Contract to Accept Payment and Withdraw it using Truffle",
+      lecturesCount: "5",
+      totalDuration: "48 mins",
+      lecture: [
+        {
+          title: "Install Truffle & Ganache",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmZzvifGbfcjFtUVbzHJVyGDE7azw4y4VwBCnioe6VLw4R",
+          usersCompleted: [],
+        },
+        {
+          title: "Writing The Smart Contract",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmQja6JC2uA7BerGCDEkrea1B5hFxBQjSQxcYoHFm4u8uz",
+          usersCompleted: [],
+        },
+        {
+          title: "Compile & Deploy to Ganache",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmaqYc2AnT6bk1FXCbHMpzeJ3HqUTRiySTMTnnPnMyZQe6",
+          usersCompleted: [],
+        },
+        {
+          title: "Write Test For Payable Smart Contract",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmRZcHvYPMYRz9M8gp6efPQYw48gbjfksqmPRxCY85rce2",
+          usersCompleted: [],
+        },
+        {
+          //TODO Video Url Not Available
+          title: "How to Debug Smart Contract Using Truffle",
+          desc: "",
+          videoUrl: ".",
+          usersCompleted: [],
+        },
+      ],
+    },
+
+    //Building a frontend integration with Metamask and trust wallet
+    {
+      title: "Building a frontend integration with Metamask and trust wallet",
+      lecturesCount: "4",
+      totalDuration: "56 mins",
+      lecture: [
+        {
+          title: "Deploy on Rinkeby Via Infura & verify Contract",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmXDdC1gFVdoY74bbbMNAC6o2AnnwySM9P4FHqcZ1pbbsQ",
+          usersCompleted: [],
+        },
+        {
+          title: "Deploy on BSC Main & Verify Contract",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmXbuV7nT5XJGbu4QdB6TXrwZgVrj9yQHMMynoCjhfKtLp",
+          usersCompleted: [],
+        },
+        {
+          title: "Ethers.js & Bootstrap to Build UI",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmdAHE79iCfT2Q4dzYiEWaD3HXYr893WSUrpk91PYemCvJ",
+          usersCompleted: [],
+        },
+        {
+          title: "Interact with Smart Contract with UI Demo",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmUsMSKyX9TEuVrTQ2U8crELR8EZg965JxSq2xn71Hs31r",
+          usersCompleted: [],
+        },
+      ],
+    },
+
+    //Write a Smart Contract that you can Upgrade after Deploying on the Blockchain
+    {
+      title: "Write a Smart Contract that you can Upgrade after Deploying on the Blockchain",
+      lecturesCount: "3",
+      totalDuration: "19 mins",
+      lecture: [
+        {
+          title: "How Upgrades Work",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmZ9NcvzMpJUuELz9JQNkeybTY3CaiBmhRtPQPqUoRpDvH",
+          usersCompleted: [],
+        },
+        {
+          title: "Redeploy Version 1 Payable Contarct to Ganache",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmSkXekcENeyQUFYDjXF75MkEy3vxL34uSejpsodVD9mAB",
+          usersCompleted: [],
+        },
+        {
+          title: "Redeploy Version 2 Payable Contract to Ganache",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/Qme7AqdggNLJCg3cvGFcL5CBT78vpfr6dhBELfvh4hABTX",
+          usersCompleted: [],
+        },
+      ],
+    },
+
+    // Build a Defi Smart Contract that Generate Yield using the Compound Supply Market
+    {
+      title: "Build a Defi Smart Contract that Generate Yield using the Compound Supply Market",
+      lecturesCount: "8",
+      totalDuration: "1 hr 23 mins",
+      lecture: [
+        {
+          title: "What is Compound Protocol",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmWVqaKHSiL3XH5UfSjXduPadC345C8h7vVpY7v7H2noTe",
+          usersCompleted: [],
+        },
+        {
+          title: "Writing Smart Contract Part I",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmUNC9ZHija63v6U9Snhk3qXVjM4QdQaRK3smHtbVJUeS4",
+          usersCompleted: [],
+        },
+        {
+          title: "Writing Smart Contract Part II",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmX4RZXPGbngssQEcV1q6jGbX3qmohHNnSFnWfrbBXnCCA",
+          usersCompleted: [],
+        },
+        {
+          title: "Mint Test Dai",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmaGcMnJPrKn4X9gqW3yvUBeobPhVxDHg2ajvSkSG5ndG9",
+          usersCompleted: [],
+        },
+        {
+          title: "Testing Our Smart Contract",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmaKXfWAXPi5g4bYdwDCxzRUcJFPgJx7nxmCBo3MbaQJnG",
+          usersCompleted: [],
+        },
+        {
+          title: "Interact with Smart Contract as an API Using Curl",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmagBk4yyVG9FdE7UAWkmrcHa4njVsHXk2xm9fURKssBrt",
+          usersCompleted: [],
+        },
+        {
+          title: "Conclusion",
+          desc: "",
+          videoUrl: "https://gateway.lighthouse.storage/ipfs/QmaacXm7H7Eqj93sV1i3Js46LTKYbk8u4S4wNoZAcHArLV",
+          usersCompleted: [],
+        },
+      ],
+    },
   ],
 };
